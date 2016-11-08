e5e1d8857f26facbcfa53fcaccdc619f485410c7
wanglong <master@wanglong.name>
2016-11-08 21:58:36 +0800

    支持延迟动作，添加debug调试点

--- js/inject.js
+++ js/inject.js
@@ -1,90 +1,118 @@
-var url = window.location.href;
-var doc = $(document);
+jQuery.noConflict();
+; (function ($) {
+    // 填充间隔速率（毫秒）
+    const INTERVAL = 10;
+    // 当前页面网址
+    const URL = window.location.href;
 
-console.log($)
+    var doc = $(document);
 
-function xpath(path) {
-    try {
-        return doc.xpath(path);
+    function log(msg, data, color) {
+        color = color || 'yellow';
+        console.group("%c" + msg + ":", "background:" + color + ";");
+        console.log(data);
+        console.groupEnd();
     }
-    catch (e) {
-        e.xpath = path;
-        console.log(e);
-        return null;
+
+    function xpath(path) {
+        try {
+            return doc.xpath(path);
+        }
+        catch (e) {
+            e.xpath = path;
+            log("XPath语法错误", e);
+            return null;
+        }
     }
-}
 
-function applyScope(scope, dataNgModel, value) {
-    var arr = dataNgModel.split('.');
-    console.log(arr)
-    var tmp = scope;
-    for (var i=0;i<arr.length-1;i++) {
-        tmp = tmp[arr[i]];
+    // 绑定angular数据
+    function applyScope(scope, dataNgModel, value) {
+        var arr = dataNgModel.split('.');
+        for (var i = 0; i < arr.length - 1; i++) {
+            scope = scope[arr[i]];
+        }
+        scope[arr[arr.length - 1]] = value;
     }
-    
-    tmp[arr[arr.length-1]] = value;
-}
 
-function execRoles(roles) {
-    var $activeElement = $(document.activeElement);
+    // 执行规则
+    function execRoles(roles) {
+        var $activeElement = $(document.activeElement);
+        log("当前焦点元素", $activeElement.eq(0) || null);
 
-    for (var key in roles) {
-        var role = roles[key];
-        var rurl = role.url;
-        rurl = rurl.replace(/\./g, '\\.');
-        rurl = rurl.replace(/\*/g, '[a-zA-Z_-]*');
-        rurl = '^' + rurl;
+        for (var key in roles) {
+            var role = roles[key];
+            var rurl = role.url;
+            rurl = rurl.replace(/\./g, '\\.');
+            rurl = rurl.replace(/\*/g, '[a-zA-Z_-]*');
+            rurl = '^' + rurl;
 
-        var pattern = new RegExp(rurl);
+            var pattern = new RegExp(rurl);
 
-        // 与当前URL匹配
-        if (pattern.exec(url)) {
-            if (role.focus != "" && xpath(role.focus) && !xpath(role.focus).is($activeElement)) {
-                continue;
-            }
-
-            var arr = role.content;
-            arr = arr.reverse();
-
-            for (var i in arr) {
-                var obj = arr[i];
-                console.log(obj);
-
-                if (obj.value == '{click}') {
-                    var xo = xpath(obj.selector);
-                    if (xo) {
-                        xo.each(function () {
-                            this.click();
-                        });
-                    }
+            // 与当前URL匹配
+            if (pattern.exec(URL)) {
+                log("URL匹配成功", URL);
+                if (role.focus != "" && xpath(role.focus) && !xpath(role.focus).is($activeElement)) {
+                    log("Focus匹配失败", role.focus);
+                    continue;
                 }
-                else {
-                    if (xpath(obj.selector)) {
-                        var com = xpath(obj.selector);
-                        com.val(obj.value);
 
-                        // 使用Angular的项目尝试进行数据绑定
-                        if (angular) {
-                            var dataNgModel = com.attr('data-ng-model');
-                            console.log(dataNgModel)
-                            var dom = com[0];
-                            var scope = angular.element(dom).scope();
-                            applyScope(scope, dataNgModel, obj.value);
+                var arr = role.content;
+                arr = arr.reverse();
+
+                var i = 0;
+                var timer = setInterval(function () {
+                    if (i >= arr.length) {
+                        clearInterval(timer);
+                        return false;
+                    }
+
+                    var obj = arr[i];
+
+                    if (obj.value == '{click}') {
+                        var xo = xpath(obj.selector);
+                        if (xo) {
+                            xo.each(function () {
+                                log("点击元素", this);
+                                this.click();
+                            });
                         }
                     }
-                }
+                    else {
+                        var com = xpath(obj.selector);
+                        if (com && com.length > 0) {
+                            log("填充元素", obj);
+                            com.val(obj.value);
+
+                            // 使用Angular的项目尝试进行数据绑定
+                            if (angular) {
+                                var dataNgModel = com.attr('data-ng-model');
+                                if (dataNgModel) {
+                                    log("Angular绑定", dataNgModel);
+                                    var dom = com[0];
+                                    var scope = angular.element(dom).scope();
+                                    applyScope(scope, dataNgModel, obj.value);
+                                }
+                            }
+                        }
+                        else {
+                            log("没有找到填充元素", obj, 'red');
+                        }
+                    }
+
+                    i++;
+                }, INTERVAL);
             }
         }
     }
-}
 
-function keyEvent(e) {
-    console.log(e)
-    if (e.shiftKey == true && e.key == "Enter") {
-        var data = document.getElementById("data").innerText;
-        var data = JSON.parse(data);
-        execRoles(data.roles);
+    function keyEvent(e) {
+        if (e.shiftKey == true && e.key == "Enter") {
+            log('按下了激活键', e.key);
+            var data = document.getElementById("chrome_plugin_autobrush_mid_data").innerText;
+            data = JSON.parse(data);
+            execRoles(data.roles);
+        }
     }
-}
 
-doc.keypress(keyEvent);
+    doc.keypress(keyEvent);
+})(window.jQuery);
--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 $(function () {
     // 获取当前浏览器URL
     var url = window.location.href;
-    
+
     // 读取本地存储数据
     chrome.storage.local.get("roles", function (obj) {
         var data = JSON.stringify(obj);
-        $("body").append("<div id='data' style='display:none;'>"+data+"</div>"); 
+        $("body").append("<div id='chrome_plugin_autobrush_mid_data' style='display:none;'>" + data + "</div>");
 
         var jquery = chrome.runtime.getURL("js/jquery.js");
         var jqueryXPath = chrome.runtime.getURL("js/jquery.xpath.js");
         var inject = chrome.runtime.getURL("js/inject.js");
 
-        $.getScript(jquery, function(){
-            $.getScript(jqueryXPath, function(){
+        $.getScript(jquery, function () {
+            $.getScript(jqueryXPath, function () {
                 $.getScript(inject);
             });
         });
-        
-        // $("body").append("<script type='text/javascript' src='"+ jquery +"'></script>"); 
-        // $("body").append("<script type='text/javascript' src='"+ jqueryXPath +"'></script>"); 
-        // $("body").append("<script type='text/javascript' src='"+ inject +"'></script>"); 
     });
 });
